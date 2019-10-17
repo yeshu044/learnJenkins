a788f4a3e13048f0a7b27c036812cf4689f907d4
Yeshwanth LN <yeshwanth.l@triconinfotech.com>
2019-10-17 12:02:18 +0530

    maven added

---
+++ jenkins/src/main/java/com/example/jenkins/Controller/Controller.java
@@ -0,0 +1,18 @@
+package com.example.jenkins.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+  
+  @RequestMapping(value = "/learn", method = RequestMethod.GET )
+  public String get()
+  {
+    return "hello world";
+  }
+  
+ 
+
+}
